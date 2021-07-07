ff7406c30bfbd2f2b37026361909cdf105daf0f8
Masha Evtihevich <evtihevich1996@gmail.com>
2021-07-07 19:16:37 +0300

    feat: add the loader task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Loader from './Loader'
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <Loader />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
